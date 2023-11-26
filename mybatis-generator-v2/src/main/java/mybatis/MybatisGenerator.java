168768ced301cf10fcccafab444bddbba7a91ef3
胡睿毅 <807776962@qq.com>
2023-11-26 21:33:50 +0800

    backup

--- mybatis-generator-v2/src/main/java/mybatis/MybatisGenerator.java
+++ mybatis-generator-v2/src/main/java/mybatis/MybatisGenerator.java
@@ -1,14 +1,13 @@
 package mybatis;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MybatisGenerator {
     public static void main(String[] args) throws Exception {
